07e64a3fbdefc8428bfcd9452fe50e186ca7c48f
William Huang <willh4k@gmail.com>
2022-10-29 18:00:57 -0400

    custom pipe for tags

---
+++ src/app/tags.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'tags'
+})
+export class TagsPipe implements PipeTransform {
+
+  transform(value: any[], ...args: unknown[]): unknown {
+    let retVal = "";
+    for (let i=0; i<value.length; i++) {
+      retVal += value[i]["name"] + ", "
+    }
+    return retVal;
+  }
+
+}
